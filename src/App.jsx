32682cd723c1eaa8bda05587f0025c7387bd648e
surya <rushiveerasaisurya@gmail.com>
2025-10-22 13:55:58 +0530

    resolved issues in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/games_hub/">
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
